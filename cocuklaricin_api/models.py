e994670f2c6b7f88989b49b56f2501fd5a85d3c9
orkunasa <orkunasa@gmail.com>
2019-01-13 23:10:40 +0300

    Settings abstraction is done and models are created.

---
+++ cocuklaricin_api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Machine(models.Model):
+    nick_name = models.CharField(blank=True, max_length=200)
+    address = models.CharField(blank=False, null=False, max_length=1000)
+    map_url = models.URLField(blank=False, null=False)
+    fund_raise_date = models.DateField(blank=False, null=False)
+    campaign = models.ForeignKey('Campaign', on_delete=models.CASCADE)
+    fund = models.ForeignKey('fund', on_delete=models.CASCADE)
+
+class Fund(models.Model):
+    amount = models.FloatField(blank=False)
+    cancel_time = models.DateTimeField(blank=True, null=True)
+    time = models.DateTimeField(blank=False, null=False)
+    is_canceled = models.BooleanField(default=False)
+
+class Campaign(models.Model):
+    start_date = models.DateTimeField(blank=False, null=False)
+    end_date = models.DateTimeField(blank=True, null=True)
+    notes = models.TextField(blank=True)
+
+class CompanyExecutive(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=50)
+    surname = models.CharField(blank=False, null=False, max_length=15)
+    gsm_no = models.CharField(blank=False, null=False, max_length=25)
+    campaign = models.ForeignKey('Campaign', on_delete=models.CASCADE)
